8f43605a3a2a25fad654e3a538d354373eb4382c
Cody Crawford <cody@griffintechnology.com>
2012-01-05 11:10:06 -0600

    second code commit

--- code.c
+++ code.c
@@ -1,1 +1,20 @@
 // add code here
+
+void main(void)
+{
+   int number = 0;
+   int x, pause;
+   
+   //string pause;
+   
+   while (number < 5)
+   {
+      printf("%d\n", number);
+      number++;
+      
+   }
+   
+   pause = scanf("%d", &x);
+   
+}
+      
